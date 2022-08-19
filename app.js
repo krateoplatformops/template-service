ad197e103df3ed2c6aa1bff546321302d1aaad16
mauro sala <mauro.sala@gmail.com>
2022-08-19 15:09:04 +0200

    feat(app): list middleware

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ app.use(responseTime({ suffix: false, digits: 0 }))
 
 /* Middlewares */
 const callLoggerMiddleware = require('./service-library/middlewares/call-logger.middleware')
+const listMiddleware = require('./service-library/middlewares/list.middleware')
 const errorLoggerMiddleware = require('./service-library/middlewares/error-logger.middleware')
 
 app.use(callLoggerMiddleware)
+app.use(listMiddleware)
 
 /* Routes */
 const statusRoutes = require('./service-library/routes/status.routes')
