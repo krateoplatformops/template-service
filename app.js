69a4a2c19b295cfab89497c13e404f62f40c09ee
mauro sala <mauro.sala@gmail.com>
2022-10-21 11:15:57 +0200

    fix(app): k8s.init port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')({ origin: true, credentials: true })
 const responseTime = require('response-time')
 require('dotenv').config()
+const k8sHelpers = require('./service-library/helpers/k8s.helpers')
+k8sHelpers.init()
 
 const app = express()
 app.use(cors)
